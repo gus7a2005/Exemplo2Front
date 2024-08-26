e6f70c57524783243ca2b572576d7cae7c57b370
Gustavo <gumorenocoelho@gmail.com>
2024-08-26 11:48:36 -0300

    Criando rotas de error, footer e nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import {Outlet} from 'react-router-dom'
 function App() {
 
 
   return (
     <>
+      <Nav/>
+      {/* CHAMANDO TODOS OS ELEMENTOS FILHOS */}
+      <Outlet/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import {Link} from 'react-router-dom'
+const Nav=()=>{
+    return(
+        <>
+        <Link to='/'>Home</Link>
+        <Link to='/aluno'>Aluno</Link>
+        </>
+    )
+}
+export default Nav
